8ea790c3c9df986b32f491436b0453e94ba55939
Roman Poliakov <roman61394@gmail.com>
2022-04-22 22:18:28 +0300

    minor chancges to brain-gcd

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -7,6 +7,6 @@ game_rules = 'Find the greatest common divisor of given numbers.'
 
 def ask_question_get_answer():
     number_1, number_2 = randint(1, 99), randint(1, 99)
-    question = f"{number_1}, {number_2}"
+    question = f"{number_1} {number_2}"
     correct_answer = str(gcd(number_1, number_2))
     return question, correct_answer
